ec4a3ef03d353a2ad726ba7df5005625561f7758
Arundel <9891546+darthglowball@users.noreply.github.com>
2022-03-14 19:25:24 +0100

    Add files via upload

---
+++ js/importer.js
@@ -0,0 +1,13 @@
+function importHTML(code){
+    let codeFragment = document.createElement("div");
+    let script = document.currentScript;
+    codeFragment.innerHTML = code;
+    script.parentNode.insertBefore(codeFragment, script);
+    while (codeFragment.childNodes.length > 0){
+        let item = codeFragment.childNodes[0];
+        console.log(item);
+        script.parentNode.insertBefore(item, script);
+    };
+    codeFragment.remove();
+    script.remove();
+};
