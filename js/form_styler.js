2d6fcc94d2b8a60c38d6eb6bcd85715490e941e7
B Evelo <bart.evelo@gmail.com>
2022-03-16 20:29:48 +0100

    submit button fixed (again lmao)

--- js/form_styler.js
+++ js/form_styler.js
@@ -17,10 +17,35 @@ function registerEvents(e){
     };
     selectContainer.appendChild(selector);
     //Add event listeners to all buttons
-    document.getElementsByClassName("form__submitbtn").addEventListener("click", changeStyling, false);
+    document.querySelector(".form__submitbtn").addEventListener("click", changeStyling, false);
     document.querySelector(".changeAppearenceBtn").addEventListener("click",changeStylerFormVisibility);
     document.querySelector(".styler-form__closeBtn").addEventListener("click",changeStylerFormVisibility);
 }
+
+function changeStyling(e){
+    e.preventDefault();
+    //Get all values from the form
+    var stylerForm = document.querySelector(".styler-form");
+    var backgroundColor = stylerForm.elements["backgroundColor"].value;
+    var fontSize = stylerForm.elements["fontSize"].value;
+    var fontColor = stylerForm.elements["fontColor"].value;
+    var borderSize = stylerForm.elements["borderSize"].value;
+    var borderColor = stylerForm.elements["borderColor"].value;
+    var borderRadius = stylerForm.elements["borderRadius"].value;
+    var selectedElement = document.querySelector(".form__selector").value;
+    var allSelectedElements = document.querySelectorAll(selectedElement);
+
+    //Change the style of selected elements
+    for(let i = 0; i < allSelectedElements.length; i++){
+        allSelectedElements[i].style.color = fontColor;
+        allSelectedElements[i].style.fontSize = fontSize;
+        allSelectedElements[i].style.border = borderSize + " solid " + borderColor;
+        allSelectedElements[i].style.borderRadius = borderRadius;
+        allSelectedElements[i].style.backgroundColor = backgroundColor;
+    };
+    changeStylerFormVisibility(e);
+};
+
 
 function searchChildren(parent){
     // Check if parent is a header, footer,article,section or aside
@@ -39,29 +64,7 @@ function searchChildren(parent){
 };
 
 
-function changeStyling(e){
-    e.preventDefault();
-    //Get all values from the form
-    var stylerForm = document.querySelector(".styler-form");
-    var backgroundColor = stylerForm.elements["backgroundColor"].value;
-    var fontSize = stylerForm.elements["fontSize"].value;
-    var fontColor = stylerForm.elements["fontColor"].value;
-    var borderSize = stylerForm.elements["borderSize"].value;
-    var borderColor = stylerForm.elements["borderColor"].value;
-    var borderRadius = stylerForm.elements["borderRadius"].value;
-    var selectedElement = selector.value;
-    var allSelectedElements = document.querySelectorAll(selectedElement);
 
-    //Change the style of selected elements
-    for(let i = 0; i < allSelectedElements.length; i++){
-        console.log(allSelectedElements[i].tagName);
-        allSelectedElements[i].style.color = fontColor;
-        allSelectedElements[i].style.fontSize = fontSize;
-        allSelectedElements[i].style.border = borderSize + " solid " + borderColor;
-        allSelectedElements[i].style.borderRadius = borderRadius;
-        allSelectedElements[i].style.backgroundColor = backgroundColor;
-    };
-};
 
 function changeStylerFormVisibility(e){
     e.preventDefault();
