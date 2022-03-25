2774c175641b6b53bb6ca9cd0c295db0fd234d16
Arundel <9891546+darthglowball@users.noreply.github.com>
2022-03-25 22:41:58 +0100

    navigation presentation logic

--- js/navigation.js
+++ js/navigation.js
@@ -15,11 +15,21 @@ importHTML(`
         -->
     </nav>
     <div id="navbar__login-container">
-        <script>
-            
-        </script>
-        <a id="navbar__login-link" href="../login/index.html">Login</a>
-        <a id="navbar__user-link" href="../user/index.html">Profile</a>
+        <a id="navbar__login-link" style="visibility: hidden" href="../login/index.html">Login</a>
+        <a id="navbar__user-link" style="visibility: hidden" href="../user/index.html">Profile</a>
     </div>
+    <script> 
+        // Presentation Logic
+        fetch("actions/getUserLoggedIn()").then(result => {
+            if (result.body === "null"){
+                $("navbar__login-link").style.visibility = "visible";
+                $("navbar__user-link").style.visibility = "hidden";
+            } else {
+                $("navbar__login-link").style.visibility = "hidden";
+                $("navbar__user-link").style.visibility = "visible";
+                $("navbar__user-link").textContent = result.body + "'s Profile";
+            };
+        })
+    </script>
 </header>
 `)
