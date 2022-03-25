33bc5e35e327aaefc2c6f9b4f1f13b962d026a15
Arundel <9891546+darthglowball@users.noreply.github.com>
2022-03-25 23:30:54 +0100

    navigation presentation logic

--- js/navigation.js
+++ js/navigation.js
@@ -20,16 +20,24 @@ importHTML(`
     </div>
     <script> 
         // Presentation Logic
-        fetch("actions/getUserLoggedIn()").then(result => {
-            if (result.body === "null"){
+        fetch("actions/getUserLoggedIn()")
+        .then(result => {
+            if (result.ok){
+                return result.json();
+            } else {
+                $("navbar__login-container").textContent = "Error: can't get user login state."
+            };
+        })
+        .then(data => {
+            if (data.name === "null"){
                 $("navbar__login-link").style.visibility = "visible";
                 $("navbar__user-link").style.visibility = "hidden";
             } else {
                 $("navbar__login-link").style.visibility = "hidden";
                 $("navbar__user-link").style.visibility = "visible";
-                $("navbar__user-link").textContent = result.body + "'s Profile";
+                $("navbar__user-link").textContent = data.name + "'s Profile";
             };
-        }).catch(()=>{ $("navbar__login-container").textContent = "Error: can't get user login state." });
+        });
     </script>
 </header>
 `)
