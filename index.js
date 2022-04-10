42aba67f5444915c4c5cf627553107de4c7586bf
Arundel <9891546+darthglowball@users.noreply.github.com>
2022-04-10 22:33:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ let session = require("express-session");
 const fs = require("fs");
 const path = require('path');
 
+let app = express();
+let router = express.Router();
+
 const logger = require('./middleware/logger');
 const defaultErrorHandler = require('./middleware/defaultErrorHandler');
 const db = require("./server/database");
+const routing = require("./routing/index.js");
 
-let app = express();
-let router = express.Router();
+
 
 const PORT = 8060
 
@@ -49,74 +52,6 @@ function getUserLoggedIn(sessionId){
     let userInfo = database.getCells("Users", null, {"sessionId": sessionId});
     return JSON.stringify(userInfo); // if there's a user with the sessionId, they're logged in, so return some info about them.
 };
-
-
-
-
-// Database requests from client.
-// Any page
-/* router.get("/:rest", (error, request, response, next) => {
-    if (request.session.id){ // active session found.
-        request.params.rest, request.method, request.body
-    };
-}); */
-
-router.get("/menu/index.html", (error, request, response, next) => {
-    if (request.session.id){
-        let user = getUserLoggedIn(request.session.id);
-        if (user){ // user is logged in.
-            
-        } else { // user is anonymous.
-            
-        };
-    };
-});
-
-
-// Actions from client.
-
-router.get("actions/getUserLoggedIn", (error, request, response, next) => {
-    response.send(JSON.stringify(getUserLoggedIn(request.session.id)));
-});
-
-
-router.post("actions/loginUser", (error, request, response, next) => { // only the corresponding form should navigate here.
-    if (!isMultiStringInputClean(request.body)){
-        next(new Error("actions/loginUser: sanitization check failed; one of the inputs is dirty"));
-    };
-    // perform password check by database or javascript??
-    let user = database.getCells(
-        "Users",
-        ["username", "password"],
-        [
-            "username=${request.body.username} AND",
-            "password=${request.body.password}"
-        ],
-        next
-    );
-    if (user){ // successfull login.
-        response.redirect("home");
-    };
-});
-
-
-router.post("actions/registerUser", (error, request, response, next) => {
-    if (!isMultiStringInputClean(request.body)){
-        next(new Error("actions/registerUser: sanitization check failed; one of the inputs is dirty"));
-    };
-    database.pushRecord(
-        "Users",
-        {
-            "username":  request.body.username, 
-            "password":  request.body.password,
-            "town":      request.body.town,
-            "address":   request.body.address,
-            "sessionId": request.session.id
-        }, 
-        next
-    );
-});
-
 
 app.use(defaultErrorHandler);
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
